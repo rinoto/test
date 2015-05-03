ff3dd4f32cdf67204aefdcdf8e3be83c0fcfcb4d
Ruben Inoto <rinoto@real.com>
2015-05-03 17:38:54 +0200

    a

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-test
-====
+# Java8
 
-lala
+## Collections vs Streams
+
+Collection is an *in-memory* data structure: all the items of the Collection are stored in memory. To read elements from a Collection we use a *pull* model: we ask the Collection to give us the next element.
+
+In a Stream, elements are computed *on demand*. Kind of lazy constructed Collection. When reading elements from an Stream, a *push* model is used: when elements are available, the stream call us to perform an operation on the element.
+
+## Lambdas
+
+### Anonymous functions 
--- src/main/java/org/inru/java8/Demo.java
+++ src/main/java/org/inru/java8/Demo.java
@@ -4,31 +4,85 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Demo {
-	
+
 	public static void filter() {
-		
+
 		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
-		
-		//java
+
+		// java
 		List<Integer> filteredList = new ArrayList<Integer>();
 		for (Integer i : list) {
-			if (i<3) {
+			if (i < 3) {
 				filteredList.add(i);
 			}
 		}
 		System.out.println(filteredList);
 
-		
-		//java8
-		List<Integer> collect = list.stream().filter(i -> i<3).collect(Collectors.toList());
+		// java8
+		List<Integer> collect = list.stream().filter(i -> i < 3).collect(Collectors.toList());
 		System.out.println(collect);
-		
+
 	}
-	
+
+	public static void map() {
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+		// java
+		List<Integer> plusOneList = new ArrayList<Integer>();
+		for (Integer i : list) {
+			plusOneList.add(i + 1);
+		}
+		System.out.println(plusOneList);
+
+		// java8
+		List<Integer> collect = list.stream().map(i -> i + 1).collect(Collectors.toList());
+		System.out.println(collect);
+
+	}
+
+	public static void flatMap() {
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+		// java
+		List<List<Integer>> elementAndPlusOneListOfLists = new ArrayList<List<Integer>>();
+		for (Integer i : list) {
+			elementAndPlusOneListOfLists.add(Arrays.asList(i, i + 1));
+		}
+		List<Integer> flatList = new ArrayList<Integer>();
+		for (List<Integer> integerSubList : elementAndPlusOneListOfLists) {
+			flatList.addAll(integerSubList);
+		}
+		System.out.println(flatList);
+
+		// java8
+		List<Integer> collect = list.stream().flatMap(i -> Stream.of(i, i + 1)).collect(Collectors.toList());
+		System.out.println(collect);
+
+	}
+
+	public static void streamIsLazy() {
+
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+		// java8
+		Stream<Integer> stream = list.stream().map(i -> {
+			System.out.println("element: " + i);
+			return i;
+		});
+		System.out.println("stream has been created");
+		System.out.println("start collecting");
+		stream.collect(Collectors.toList());
+		System.out.println("Collect finished");
+
+	}
+
 	public static void main(String[] args) {
-		filter();
+		streamIsLazy();
 	}
 
 }
